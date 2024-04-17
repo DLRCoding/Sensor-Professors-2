9c8f8379caed6f6cf00969e2b77b1942ffe62c9f
DLRCoding <167281718+DLRCoding@users.noreply.github.com>
2024-04-17 00:55:36 -0500

    Update PrototypeCode.ino
    Added comments to certain parts

--- PrototypeCode.ino
+++ PrototypeCode.ino
@@ -11,7 +11,7 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(touchPin, INPUT);
   servoMotor.attach(9);
-  servoMotor.write(0);
+  servoMotor.write(0); // sets the servo angle to start at 0 degrees
 }
 
 void loop() {
@@ -22,12 +22,12 @@ void loop() {
         digitalWrite(ledPin, HIGH);
         ledOn = true;
       }
-      servoMotor.write(180);
+      servoMotor.write(180); // rotates the servo to 180 degrees when the circuit is completed
       delay(100);
     }
   } else {
     if (isTouched) {
-      isTouched = false;
+      isTouched = false; // detects whenever the circuit is not touching to turn off the led and reset the servo to it's original position
       if (ledOn) {
         digitalWrite(ledPin, LOW);
         ledOn = false;
